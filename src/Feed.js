cef574d9bc0ecdd816601b8b1f597951937fa40e
Jonathan White <contact@jonathancwhite.com>
2021-10-19 23:30:01 -0500

    trying to fix firebase import

--- src/Feed.js
+++ src/Feed.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { db } from './firebaseConfig';
+import { db } from './firebase';
 import Filters from './Filters';
 import Posts from './Posts';
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase';
-import { initializeApp } from 'firebase';
+import { initializeApp } from "firebase";
 import 'firebase/firestore';
 
 const firebaseConfig = {
